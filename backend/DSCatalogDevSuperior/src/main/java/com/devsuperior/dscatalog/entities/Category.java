34e5588196a1c00fe657ecf59e043471427da7b9
VanssaJaniszewski <vjaniszewski@gmail.com>
2022-12-16 11:08:51 -0300

    H2 teste database added

--- backend/DSCatalogDevSuperior/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/DSCatalogDevSuperior/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,12 +2,18 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+@Entity
+@Table(name = "tb_category")
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -17,6 +23,7 @@ public class Category implements Serializable{
 	
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
 	private Long id;
 	private String name;

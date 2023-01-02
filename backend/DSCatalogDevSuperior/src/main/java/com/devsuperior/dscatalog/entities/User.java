164822043040bd6eacf3b22bc20aa78dd212d8ca
VanssaJaniszewski <vjaniszewski@gmail.com>
2023-01-02 13:10:15 -0300

    authorization

--- backend/DSCatalogDevSuperior/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/DSCatalogDevSuperior/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.entities;
 
+import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,7 +17,9 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-import com.devsuperior.dscatalog.dto.RoleDTO;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -28,8 +33,9 @@ import lombok.Setter;
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "tb_user")
-public class User {
-
+public class User implements UserDetails,Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@Column(name = "user_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -46,6 +52,37 @@ public class User {
 	 	 		joinColumns = @JoinColumn(name = "user_id"),
 	 	 		inverseJoinColumns = @JoinColumn (name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 	
 
 	
